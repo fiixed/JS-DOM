216d0bd143f3ccba6aea76fab62b83c8bf4b9037
fiixed <fiixed@gmail.com>
2020-09-25 17:03:33 -0500

    ex5 and ex6

--- ex5/script.js
+++ ex5/script.js
@@ -2,10 +2,8 @@
 // HINT: Look up createElement(), appendChild()
 // HINT HINT: You can add the "box" class to elements like so: https://www.w3schools.com/howto/howto_js_add_class.asp
 var element = document.getElementById("boxContainer");
+
 document.querySelector("#myButton").addEventListener("click", function () {
-  document
-    .getElementById("boxContainer")
-    .createElement("div")
-    .className(box)
-    .appendChild();
+  var div = document.createElement("div");
+  element.appendChild(div).classList.add("box");
 });
