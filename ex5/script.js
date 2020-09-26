6ca1e222558810175f469e10fd30021f971998c5
fiixed <fiixed@gmail.com>
2020-09-26 11:28:45 -0500

    minor edit to ex7

--- ex5/script.js
+++ ex5/script.js
@@ -1,9 +1,14 @@
+/*jshint esversion: 6 */
 // Add a box to "boxContainer" everytime you click the button
 // HINT: Look up createElement(), appendChild()
 // HINT HINT: You can add the "box" class to elements like so: https://www.w3schools.com/howto/howto_js_add_class.asp
-var element = document.getElementById("boxContainer");
+//var element = document.getElementById("boxContainer");
 
-document.querySelector("#myButton").addEventListener("click", function () {
-  var div = document.createElement("div");
-  element.appendChild(div).classList.add("box");
-});
+document
+  .querySelector("#myButton")
+  .addEventListener("click", () =>
+    document
+      .querySelector("#boxContainer")
+      .appendChild(document.createElement("div"))
+      .classList.add("box")
+  );
--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,14 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+var header = document.getElementById("section-header");
+var section = document.getElementById("section");
+header.addEventListener("click", function () {
+  section.style.visibility == "visible" || !section.style.visibility
+    ? (section.style.visibility = "hidden")
+    : (section.style.visibility = "visible");
+});
+
+// header.addEventListener("click", function () {
+//   section.classList.add("collapse");
+// });
